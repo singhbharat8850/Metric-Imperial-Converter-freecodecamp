53f0db7dfc0284fcbe973cea2a1caa3551f5671f
Zi Wei Zhang <ziwei.lotus@gmail.com>
2018-09-16 06:34:33 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,44 +1,48 @@
-/* *
+/*
+*
 *
 *       Complete the API routing below
 *
-* */
+*
+*/
 
 'use strict';
 
 var expect = require('chai').expect;
 var ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function(app) {
-
+module.exports = function (app) {
+  
   var convertHandler = new ConvertHandler();
 
-  app.route('/api/convert').get(function(req, res) {
-    var input = req.query.input;
-    var initNum = convertHandler.getNum(input);
-    var initUnit = convertHandler.getUnit(input);
-    if (initNum == 'invalid number' && initUnit != 'invalid unit') {
-      res.json(initNum);
-    } else if (initUnit == 'invalid unit' && initNum != 'invalid number') {
-      res.json(initUnit);
-    } else if (initNum == 'invalid number' && initUnit == 'invalid unit') {
-      res.json('invalid number and unit');
-    }
-
-    var returnNum = convertHandler.convert(initNum, initUnit);
-    var returnUnit = convertHandler.getReturnUnit(initUnit);
-    console.log(returnUnit);
-    var spellout = convertHandler.spellOutUnit(returnUnit);
-    var toString = convertHandler.getString(initNum, initUnit, returnNum, spellout);
-    var myObj = {
-      initNum: initNum,
-      initUnit: initUnit,
-      returnNum: returnNum,
-      returnUnit: returnUnit,
-      string: toString
-    }
-    //res.json(toString);
-    res.json(myObj);
-  });
-
+  app.route('/api/convert')
+    .get(function (req, res){
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      if (initNum == 'invalid number' && initUnit != 'invalid unit') {
+            res.json(initNum);
+          } else if (initUnit == 'invalid unit' && initNum != 'invalid number') {
+            res.json(initUnit);
+          } else if (initNum == 'invalid number' && initUnit == 'invalid unit') {
+            res.json('invalid number and unit');
+          }
+      
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);   
+      var spellout1 = convertHandler.spellOutUnit(initUnit);
+      var spellout2 = convertHandler.spellOutUnit(returnUnit);
+      var toString = convertHandler.getString(initNum, spellout1, returnNum, spellout2);
+      var myObj = {
+        initNum: initNum,
+        initUnit: initUnit,
+        returnNum: returnNum,
+        returnUnit: returnUnit,
+        string: toString
+      }
+     
+      res.json(myObj);
+       
+    });
+    
 };
